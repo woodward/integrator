18d019f88a2685c0838c52da9dcdd2e0100437d4
Greg Woodward <woodward@users.noreply.github.com>
2022-08-25 13:29:13 -0700

    Output from 'mix new momentum'

---
+++ lib/momentum.ex
@@ -0,0 +1,18 @@
+defmodule Momentum do
+  @moduledoc """
+  Documentation for `Momentum`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Momentum.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/momentum_test.exs
@@ -0,0 +1,8 @@
+defmodule MomentumTest do
+  use ExUnit.Case
+  doctest Momentum
+
+  test "greets the world" do
+    assert Momentum.hello() == :world
+  end
+end
