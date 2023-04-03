5d5ccc3ececc109340db576bd5e4b648afe7b6cb
Greg Woodward <woodward@users.noreply.github.com>
2023-04-03 12:20:52 -0700

    Change absolute_relative_norm to a defn (from def)

--- lib/integrator/integrator/utils.ex
+++ lib/integrator/integrator/utils.ex
@@ -1,42 +1,36 @@
 defmodule Integrator.Utils do
   @moduledoc false
+  import Nx.Defn
 
-  #  DELETE LATER!!!
-  #   function retval = AbsRel_norm (x, x_old, AbsTol, RelTol, normcontrol, y = zeros (size (x)))
+  @doc """
 
-  #   if (normcontrol)
-  #     sc = max (AbsTol(:), RelTol * max (sqrt (sumsq (x)), sqrt (sumsq (x_old))));
-  #     retval = sqrt (sumsq ((x - y))) / sc;
-  #   else
-  #     sc = max (AbsTol(:), RelTol .* max (abs (x), abs (x_old)));
-  #     retval = max (abs (x - y) ./ sc);
-  #   endif
+  Based on [Octave function AbsRelNorm](https://github.com/gnu-octave/octave/blob/default/scripts/ode/private/AbsRel_norm.m)
+  """
+  defn absolute_relative_norm(x, x_old, y, abs_tolerance, rel_tolerance, opts \\ []) do
+    opts = keyword!(opts, normcontrol: true)
 
-  # endfunction
+    if opts[:normcontrol] do
+      # Octave code
+      # sc = max (AbsTol(:), RelTol * max (sqrt (sumsq (x)), sqrt (sumsq (x_old))));
+      # retval = sqrt (sumsq ((x - y))) / sc;
 
-  def absolute_relative_norm(x, x_old, y, abs_tolerance, rel_tolerance, normcontrol: false) do
-    # Octave code:
-    # sc = max (AbsTol(:), RelTol .* max (abs (x), abs (x_old)));
-    # retval = max (abs (x - y) ./ sc);
+      sumsq_x = Nx.multiply(x, x) |> Nx.sum() |> Nx.sqrt()
+      sumsq_x_old = Nx.multiply(x_old, x_old) |> Nx.sum() |> Nx.sqrt()
+      max_sq_x = Nx.max(sumsq_x, sumsq_x_old)
+      rel_tol_x = Nx.multiply(rel_tolerance, max_sq_x)
+      sc = Nx.max(abs_tolerance, rel_tol_x)
 
-    sc = Nx.max(abs_tolerance, rel_tolerance |> Nx.multiply(Nx.max(Nx.abs(x), Nx.abs(x_old))))
-    Nx.subtract(x, y) |> Nx.divide(sc) |> Nx.reduce_max()
-  end
+      x_minus_y = Nx.subtract(x, y)
+      sum_sq_x_minus_y = Nx.multiply(x_minus_y, x_minus_y) |> Nx.sum() |> Nx.sqrt()
+      Nx.divide(sum_sq_x_minus_y, sc)
+    else
+      # Octave code:
+      # sc = max (AbsTol(:), RelTol .* max (abs (x), abs (x_old)));
+      # retval = max (abs (x - y) ./ sc);
 
-  def absolute_relative_norm(x, x_old, y, abs_tolerance, rel_tolerance, normcontrol: true) do
-    # Octave code:
-    # sc = max (AbsTol(:), RelTol * max (sqrt (sumsq (x)), sqrt (sumsq (x_old))));
-    # retval = sqrt (sumsq ((x - y))) / sc;
-
-    sumsq_x = Nx.multiply(x, x) |> Nx.sum() |> Nx.sqrt()
-    sumsq_x_old = Nx.multiply(x_old, x_old) |> Nx.sum() |> Nx.sqrt()
-    max_sq_x = Nx.max(sumsq_x, sumsq_x_old)
-    rel_tol_x = Nx.multiply(rel_tolerance, max_sq_x)
-    sc = Nx.max(abs_tolerance, rel_tol_x)
-
-    x_minus_y = Nx.subtract(x, y)
-    sum_sq_x_minus_y = Nx.multiply(x_minus_y, x_minus_y) |> Nx.sum() |> Nx.sqrt()
-    Nx.divide(sum_sq_x_minus_y, sc)
+      sc = Nx.max(abs_tolerance, rel_tolerance |> Nx.multiply(Nx.max(Nx.abs(x), Nx.abs(x_old))))
+      Nx.subtract(x, y) |> Nx.divide(sc) |> Nx.reduce_max()
+    end
   end
 
   def starting_stepsize() do
