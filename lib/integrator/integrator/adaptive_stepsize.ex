67a8e47ac8a24d1c9ee9753cfacd1fba13a14b61
Greg Woodward <woodward@users.noreply.github.com>
2023-04-03 16:51:36 -0700

    Implement kahan_sum/3

--- lib/integrator/integrator/adaptive_stepsize.ex
+++ lib/integrator/integrator/adaptive_stepsize.ex
@@ -1,10 +1,44 @@
 defmodule Integrator.AdaptiveStepsize do
   @moduledoc false
+  import Nx.Defn
 
   def integrate() do
   end
 
-  def kahan_summation() do
+  @doc """
+  Implements the Kahan summation algorithm, also known as compensated summation.
+  Based on this [code in Octave](https://github.com/gnu-octave/octave/blob/default/scripts/ode/private/kahan.m).
+
+  The algorithm significantly reduces the numerical error in the total
+  obtained by adding a sequence of finite precision floating point numbers
+  compared to the straightforward approach.  For more details
+  see [this Wikipedia entry](http://en.wikipedia.org/wiki/Kahan_summation_algorithm).
+  This function is called by AdaptiveStepsize.integrate to better catch
+  equality comparisons.
+
+  The first input argument is the variable that will contain the summation.
+  This variable is also returned as the first output argument in order to
+  reuse it in subsequent calls to `Integrator.AdaptiveStepsize.kahan_sum/3` function.
+
+  The second input argument contains the compensation term and is returned
+  as the second output argument so that it can be reused in future calls of
+  the same summation.
+
+  The third input argument `term` is the variable to be added to `sum`.
+  """
+  defn kahan_sum(sum, comp, term) do
+    # Octave code:
+    # y = term - comp;
+    # t = sum + y;
+    # comp = (t - sum) - y;
+    # sum = t;
+
+    y = term - comp
+    t = sum + y
+    comp = t - sum - y
+    sum = t
+
+    {sum, comp}
   end
 
   def ode_event_handler() do
--- test/integrator/integrator/adaptive_stepsize_test.exs
+++ test/integrator/integrator/adaptive_stepsize_test.exs
@@ -1,4 +1,22 @@
 defmodule Integrator.AdaptiveStepsizeTest do
   @moduledoc false
   use Integrator.TestCase
+
+  alias Integrator.AdaptiveStepsize
+
+  describe "kahan_sum" do
+    test "sums up some items" do
+      sum = Nx.tensor(2.74295650014, type: :f64)
+      comp = Nx.tensor(1.11022302463e-16, type: :f64)
+      term = Nx.tensor(0.66059601818, type: :f64)
+
+      expected_sum = Nx.tensor(3.40355251832, type: :f64)
+      expected_comp = Nx.tensor(1.11022302463e-16, type: :f64)
+
+      {sum, comp} = AdaptiveStepsize.kahan_sum(sum, comp, term)
+
+      assert_all_close(sum, expected_sum, atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(comp, expected_comp, atol: 1.0e-14, rtol: 1.0e-14)
+    end
+  end
 end
