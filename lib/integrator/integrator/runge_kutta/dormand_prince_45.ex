8ed834933c8e8b1a4bbdf40fa915fad2c410af8b
Greg Woodward <woodward@users.noreply.github.com>
2023-04-05 16:23:46 -0700

    Move the DormandPrince45 code into its own module

---
+++ lib/integrator/integrator/runge_kutta/dormand_prince_45.ex
@@ -0,0 +1,149 @@
+defmodule Integrator.RungeKutta.DormandPrince45 do
+  @moduledoc false
+
+  import Nx.Defn
+
+  @a Nx.tensor(
+       [
+         [0, 0, 0, 0, 0, 0],
+         [1 / 5, 0, 0, 0, 0, 0],
+         [3 / 40, 9 / 40, 0, 0, 0, 0],
+         [44 / 45, -56 / 15, 32 / 9, 0, 0, 0],
+         [19_372 / 6_561, -25_360 / 2187, 64_448 / 6561, -212 / 729, 0, 0],
+         [9_017 / 3_168, -355 / 33, 46_732 / 5247, 49 / 176, -5103 / 18_656, 0]
+       ],
+       type: :f64
+     )
+
+  @b Nx.tensor([0, 1 / 5, 3 / 10, 4 / 5, 8 / 9, 1, 1], type: :f64)
+  @c Nx.tensor([35 / 384, 0, 500 / 1113, 125 / 192, -2187 / 6784, 11 / 84], type: :f64)
+  @c_prime Nx.tensor([5179 / 57_600, 0, 7571 / 16_695, 393 / 640, -92_097 / 339_200, 187 / 2100, 1 / 40], type: :f64)
+
+  defn integrate(ode_fn, t, x, dt, k_vals) do
+    t_next = t + dt
+    s = t + dt * @b
+    cc = dt * @c
+    aa = dt * @a
+
+    {length_of_x} = Nx.shape(x)
+    last_k_vals_col = Nx.slice_along_axis(k_vals, 6, length_of_x - 1, axis: 1) |> Nx.flatten()
+    last_col_empty? = last_k_vals_col |> Nx.abs() |> Nx.sum() < 1.0e-04
+
+    k0 =
+      if last_col_empty? do
+        ode_fn.(t, x) |> Nx.flatten()
+      else
+        last_k_vals_col
+      end
+
+    aa_1 = Nx.slice_along_axis(aa, 1, 1) |> Nx.flatten() |> Nx.slice_along_axis(0, 1)
+    # Note that aa_1 is the also equal to aa[1][0]
+    aa_2 = Nx.slice_along_axis(aa, 2, 1) |> Nx.flatten() |> Nx.slice_along_axis(0, 2)
+    aa_3 = Nx.slice_along_axis(aa, 3, 1) |> Nx.flatten() |> Nx.slice_along_axis(0, 3)
+    aa_4 = Nx.slice_along_axis(aa, 4, 1) |> Nx.flatten() |> Nx.slice_along_axis(0, 4)
+    aa_5 = Nx.slice_along_axis(aa, 5, 1) |> Nx.flatten() |> Nx.slice_along_axis(0, 5)
+
+    k1 = ode_fn.(s[1], x + k0 * aa_1)
+    k_0_1 = Nx.stack([k0, k1])
+    k_0_1_t = k_0_1 |> Nx.transpose()
+
+    k2 = ode_fn.(s[2], x + Nx.dot(k_0_1_t, Nx.transpose(aa_2)))
+    k_0_2 = Nx.stack([k0, k1, k2])
+    k_0_2_t = k_0_2 |> Nx.transpose()
+
+    k3 = ode_fn.(s[3], x + Nx.dot(k_0_2_t, Nx.transpose(aa_3)))
+    k_0_3 = Nx.stack([k0, k1, k2, k3])
+    k_0_3_t = k_0_3 |> Nx.transpose()
+
+    k4 = ode_fn.(s[4], x + Nx.dot(k_0_3_t, Nx.transpose(aa_4)))
+    k_0_4 = Nx.stack([k0, k1, k2, k3, k4])
+    k_0_4_t = k_0_4 |> Nx.transpose()
+
+    k5 = ode_fn.(s[5], x + Nx.dot(k_0_4_t, Nx.transpose(aa_5)))
+
+    k_0_5 = Nx.stack([k0, k1, k2, k3, k4, k5])
+    k_0_5_t = k_0_5 |> Nx.transpose()
+    x_next = x + Nx.dot(k_0_5_t, cc)
+
+    k6 = ode_fn.(t_next, x_next)
+    k_new = Nx.stack([k0, k1, k2, k3, k4, k5, k6]) |> Nx.transpose()
+    cc_prime = dt * @c_prime
+    x_est = x + Nx.dot(k_new, cc_prime)
+
+    {t_next, x_next, x_est, k_new}
+  end
+
+  @coefs_u_half Nx.tensor(
+                  [
+                    6_025_192_743 / 30_085_553_152,
+                    0.0,
+                    51_252_292_925 / 65_400_821_598,
+                    -2_691_868_925 / 45_128_329_728,
+                    187_940_372_067 / 1_594_534_317_056,
+                    -1_776_094_331 / 19_743_644_256,
+                    11_237_099 / 235_043_384
+                  ],
+                  type: :f64
+                )
+
+  @doc """
+  Performs a 4th order Hermite interpolation. Used by an ODE solver to interpolate the
+  solution at the time `t_out`.
+
+  See [code in Octave](https://github.com/gnu-octave/octave/blob/default/scripts/ode/private/runge_kutta_interpolate.m#L91).
+  """
+  defn hermite_quartic_interpolation(t, x, der, t_out) do
+    # Octave code:
+    #   persistent coefs_u_half = ...
+    #   [6025192743/30085553152; 0; 51252292925/65400821598;
+    #    -2691868925/45128329728; 187940372067/1594534317056;
+    #    -1776094331/19743644256; 11237099/235043384];
+
+    # ## 4th order approximation of y in t+dt/2 as proposed by Shampine in
+    # ## Lawrence, Shampine, "Some Practical Runge-Kutta Formulas", 1986.
+    # dt = t(2) - t(1);
+    # u_half = x(:,1) + (1/2) * dt * (der(:,1:7) * coefs_u_half);
+
+    # ## Rescale time on [0,1]
+    # s = (t_out - t(1)) / dt;
+
+    # ## Hermite basis functions
+    # ## H0 = 1   - 11*s.^2 + 18*s.^3 -  8*s.^4;
+    # ## H1 =   s -  4*s.^2 +  5*s.^3 -  2*s.^4;
+    # ## H2 =       16*s.^2 - 32*s.^3 + 16*s.^4;
+    # ## H3 =     -  5*s.^2 + 14*s.^3 -  8*s.^4;
+    # ## H4 =          s.^2 -  3*s.^3 +  2*s.^4;
+
+    # x_out = (1   - 11*s.^2 + 18*s.^3 -  8*s.^4) .* x(:,1) + ...
+    #         (  s -  4*s.^2 +  5*s.^3 -  2*s.^4) .* (dt * der(:,1)) + ...
+    #         (      16*s.^2 - 32*s.^3 + 16*s.^4) .* u_half + ...
+    #         (    -  5*s.^2 + 14*s.^3 -  8*s.^4) .* x(:,2) + ...
+    #         (         s.^2 -  3*s.^3 +  2*s.^4) .* (dt * der(:,end));
+
+    dt = t[1] - t[0]
+
+    x_col1 = Nx.slice_along_axis(x, 0, 1, axis: 1)
+
+    u_half = x_col1 + 0.5 * dt * Nx.new_axis(Nx.dot(der, @coefs_u_half), 1)
+
+    s = (t_out - t[0]) / dt
+    s2 = s * s
+    s3 = s2 * s
+    s4 = s3 * s
+
+    x1 = (1.0 - 11.0 * s2 + 18.0 * s3 - 8.0 * s4) * x_col1
+
+    der_col_1 = Nx.slice_along_axis(der, 0, 1, axis: 1)
+    x2 = (s - 4.0 * s2 + 5.0 * s3 - 2.0 * s4) * (dt * der_col_1)
+
+    x3 = (16.0 * s2 - 32.0 * s3 + 16.0 * s4) * u_half
+
+    x_col2 = Nx.slice_along_axis(x, 1, 1, axis: 1)
+    x4 = (-5.0 * s2 + 14.0 * s3 - 8.0 * s4) * x_col2
+
+    der_last_col = Nx.slice_along_axis(der, 6, 1, axis: 1)
+    x5 = (s2 - 3.0 * s3 + 2.0 * s4) * (dt * der_last_col)
+
+    x1 + x2 + x3 + x4 + x5
+  end
+end
---
+++ test/integrator/integrator/runge_kutta/dormand_prince_45_test.exs
@@ -0,0 +1,115 @@
+defmodule Integrator.RungeKutta.DormandPrince45Test do
+  @moduledoc false
+  use Integrator.TestCase
+
+  import Nx, only: :sigils
+  alias Integrator.RungeKutta.DormandPrince45
+
+  describe "dormand_prince" do
+    test "gives the correct result" do
+      t = Nx.tensor(19.711, type: :f64)
+      x = Nx.tensor([1.9265, 0.7353], type: :f64)
+      dt = Nx.tensor(0.2893, type: :f64)
+
+      k_vals = ~M[
+         2.1068   1.8570   1.6938   1.0933   1.2077   1.0641   0.7353
+        -4.1080  -4.6688  -4.7104  -4.4778  -4.5190  -4.3026  -3.9202
+      ]f64
+
+      {t_next, x_next, x_est, k} = DormandPrince45.integrate(&van_der_pol_fn/2, t, x, dt, k_vals)
+
+      expected_t_next = Nx.tensor(20.0, type: :f64)
+      expected_x_next = ~V[ 2.007378 -0.071766 ]f64
+      expected_x_est = ~V[ 2.007393 -0.071892 ]f64
+
+      expected_k = ~M[
+           0.735294   0.508467   0.426833   0.026514  -0.057984  -0.116136  -0.071766
+          -3.920232  -3.432015  -3.214602  -2.106530  -1.871609  -1.681243  -1.789958
+      ]f64
+
+      assert_all_close(t_next, expected_t_next, atol: 1.0e-04, rtol: 1.0e-04)
+      assert_all_close(x_next, expected_x_next, atol: 1.0e-04, rtol: 1.0e-04)
+      assert_all_close(x_est, expected_x_est, atol: 1.0e-04, rtol: 1.0e-04)
+      assert_all_close(k, expected_k, atol: 1.0e-04, rtol: 1.0e-04)
+    end
+
+    test "gives the correct result when no existing k_vals" do
+      # Used Octave function:
+      #  [t,y] = ode45 (fvdp, [0, 20], [2, 1]);
+      # i.e., the initial values for y have been changed from [2, 0] to [2, 1]
+      # (so that it's not a zero value)
+
+      t = Nx.tensor(0.0, type: :f64)
+      x = Nx.tensor([2.0, 1.0], type: :f64)
+      dt = Nx.tensor(0.068129, type: :f64)
+
+      k_vals = ~M[
+         0.0 0.0 0.0 0.0 0.0 0.0 0.0
+         0.0 0.0 0.0 0.0 0.0 0.0 0.0
+       ]f64
+
+      {t_next, x_next, x_est, k} = DormandPrince45.integrate(&van_der_pol_fn/2, t, x, dt, k_vals)
+
+      expected_t_next = Nx.tensor(0.068129, type: :f64)
+      expected_x_next = ~V[ 2.0571  0.6839 ]f64
+      expected_x_est = ~V[ 2.0571   0.6839 ]f64
+
+      expected_k = ~M[
+           1.0000   0.9319   0.8999   0.7429   0.7162   0.6836   0.6839
+          -5.0000  -4.8602  -4.7894  -4.4195  -4.3536  -4.2690  -4.2670
+        ]f64
+
+      assert_all_close(t_next, expected_t_next, atol: 1.0e-04, rtol: 1.0e-04)
+      assert_all_close(x_next, expected_x_next, atol: 1.0e-04, rtol: 1.0e-04)
+      assert_all_close(x_est, expected_x_est, atol: 1.0e-04, rtol: 1.0e-04)
+      assert_all_close(k, expected_k, atol: 1.0e-04, rtol: 1.0e-04)
+    end
+  end
+
+  describe "hermite_quartic_interpolation" do
+    test "gives the correct result" do
+      # Values from Octave:
+      #
+      # t = [ 19.4067624192,   19.7106968201 ]
+      #
+      #  x =
+      #
+      #    1.49652504841   1.92651431954
+      #    2.10676183153   0.73529371547
+      #
+      #  der =
+      #
+      #     2.10676183153   1.85704689071   1.69384014677   1.09328301986   1.20767740745   1.06405291882   0.73529371547
+      #    -4.10804009148  -4.66882299445  -4.71039008294  -4.47781878341  -4.51898062008  -4.30261858646  -3.92023192271
+      #
+      #  t_out = [ 19.4827460194,   19.5587296196, 19.6347132198,  19.7106968201 ]
+      #
+      #  x_out =
+      #
+      #   1.64398703647   1.76488148020   1.85862355568   1.92651431954
+      #   1.77097584066   1.41075566029   1.05684789008   0.73529371547
+
+      t = ~V[ 19.4067624192  19.7106968201 ]f64
+
+      x = ~M[ 1.49652504841   1.92651431954
+              2.10676183153   0.73529371547
+            ]f64
+
+      der = ~M[
+           2.10676183153   1.85704689071   1.69384014677   1.09328301986   1.20767740745   1.06405291882   0.73529371547
+          -4.10804009148  -4.66882299445  -4.71039008294  -4.47781878341  -4.51898062008  -4.30261858646  -3.92023192271
+      ]f64
+
+      t_out = ~V[ 19.4827460194  19.5587296196  19.6347132198  19.7106968201 ]f64
+
+      expected_x_out = ~M[
+        1.64398703647   1.76488148020   1.85862355568   1.92651431954
+        1.77097584066   1.41075566029   1.05684789008   0.73529371547
+      ]f64
+
+      x_out = DormandPrince45.hermite_quartic_interpolation(t, x, der, t_out)
+
+      assert_all_close(x_out, expected_x_out, atol: 1.0e-9, rtol: 1.0e-9)
+    end
+  end
+end
