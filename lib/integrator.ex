09a47a62ed83cc435e44eaf6ef3b3b76516fa055
Greg Woodward <woodward@users.noreply.github.com>
2023-04-06 10:48:59 -0700

    First stab at getting KaTeX equations in ExDoc docs

--- lib/integrator.ex
+++ lib/integrator.ex
@@ -1,5 +1,16 @@
 defmodule Integrator do
-  @moduledoc false
+  @moduledoc """
+  $$  F = M A $$
+
+  or an inline equation via a single dollar sign, such as $ F = M A $
+
+  Some more equations:
+
+  $$ f(x) =  \\int_{-\\infty}^\\infty
+    f\\hat(\\xi)\\,e^{2 \\pi i \\xi x}
+    \\,d\\xi $$
+
+  """
   def ode45(_deriv_fn, _x_initial, _x_final, _initial_y) do
     x = [0.0, 0.0180, 0.041]
     y = [Nx.tensor([2.0000, 0.000]), Nx.tensor([1.9897, -0.0322]), Nx.tensor([1.9979, -0.0638])]
--- mix.exs
+++ mix.exs
@@ -51,9 +51,11 @@ defmodule Integrator.MixProject do
       main: "readme",
       source_url: @source_url,
       extras: [
+        "README.md",
         "guides/intro-to-integrator.livemd",
         "guides/examples-of-usage.livemd"
-      ]
+      ],
+      before_closing_body_tag: &katex_html/1
     ]
   end
 
@@ -64,5 +66,23 @@ defmodule Integrator.MixProject do
       links: %{"GitHub" => @source_url},
       files: ~w(README.md lib)
     ]
+  end
+
+  defp katex_html(:epub), do: nil
+
+  defp katex_html(:html) do
+    # From:
+    # https://katex.org/docs/browser.html
+    """
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css" integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0" crossorigin="anonymous">
+
+    <!-- The loading of KaTeX is deferred to speed up page rendering -->
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js" integrity="sha384-PwRUT/YqbnEjkZO0zZxNqcxACrXe+j766U2amXcgMg5457rve2Y7I6ZJSm2A0mS4" crossorigin="anonymous"></script>
+
+    <!-- To automatically render math in text elements, include the auto-render extension: -->
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"
+        onload="renderMathInElement(document.body);">
+    </script>
+    """
   end
 end
