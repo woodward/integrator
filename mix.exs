2ef242199c9a155098d5f36544f96f08e4f34f40
Greg Woodward <woodward@users.noreply.github.com>
2023-04-07 08:02:22 -0700

    Move the KaTeX config to separate HTML files and out of the mix file

--- mix.exs
+++ mix.exs
@@ -46,19 +46,6 @@ defmodule Integrator.MixProject do
     ]
   end
 
-  defp docs() do
-    [
-      main: "readme",
-      source_url: @source_url,
-      extras: [
-        "README.md",
-        "guides/intro-to-integrator.livemd",
-        "guides/examples-of-usage.livemd"
-      ],
-      before_closing_head_tag: &katex_js_and_css_files/1
-    ]
-  end
-
   defp package do
     [
       maintainers: ["Greg Woodward"],
@@ -68,46 +55,23 @@ defmodule Integrator.MixProject do
     ]
   end
 
-  def katex_js_and_css_files(:epub), do: ""
-
-  def katex_js_and_css_files(:html) do
-    """
-    <!--  ----------------------------------------- -->
-    <!-- From: https://hexdocs.pm/ex_doc/readme.html#rendering-math -->
-    <!-- and from: view-source:https://katex.org/ -->
-
-    <link rel="preload" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/fonts/KaTeX_Main-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/fonts/KaTeX_Math-Italic.woff2" as="font" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/fonts/KaTeX_Size2-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/fonts/KaTeX_Size4-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,700i">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css" crossorigin="anonymous">
-    <!-- do NOT include static/index.css (from view-source) -->
-
-    <script defer src="https://cdn.jsdelivr.net/npm/clipboard@2.0.1/dist/clipboard.min.js" integrity="sha256-hIvIxeqhGZF+VVeM55k0mJvWpQ6gTkWk3Emc+NmowYA=" crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js" crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/contrib/mhchem.min.js" crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/contrib/copy-tex.min.js" crossorigin="anonymous"></script>
-    <!-- do NOT include js/index.js (from view-source)
-    <script defer src="https://cdn.jsdelivr.net/npm/webfontloader@1.6.28/webfontloader.js" integrity="sha256-4O4pS1SH31ZqrSO2A/2QJTVjTPqVe+jnYgOWUVr7EEc=" crossorigin="anonymous"></script>
-
-    <!-- From: https://katex.org/docs/autorender.html#usage -->
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"
-      onload="renderMathInElement(document.body, {
-        // customised options
-        // • auto-render specific keys, e.g.:
-        delimiters: [
-            {left: '$$', right: '$$', display: true},
-            {left: '$', right: '$', display: false},
-            // {left: '\\(', right: '\\)', display: false},
-            // {left: '\\[', right: '\\]', display: true}
-        ],
-        // • rendering keys, e.g.:
-        throwOnError : false
-      } );"></script>
-    <!--  ----------------------------------------- -->
-
-    """
+  defp docs() do
+    [
+      main: "readme",
+      source_url: @source_url,
+      extras: [
+        "README.md",
+        "guides/intro-to-integrator.livemd",
+        "guides/examples-of-usage.livemd"
+      ],
+      before_closing_head_tag: &before_closing_head_tag/1,
+      before_closing_body_tag: &before_closing_body_tag/1
+    ]
   end
+
+  def before_closing_head_tag(:epub), do: ""
+  def before_closing_head_tag(:html), do: File.read!("doc/before_closing_head_tag.html")
+
+  def before_closing_body_tag(:epub), do: ""
+  def before_closing_body_tag(:html), do: File.read!("doc/before_closing_body_tag.html")
 end
