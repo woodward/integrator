00f09eafcb8180dd8951b263193d0dafdc9d1c03
Greg Woodward <woodward@users.noreply.github.com>
2023-04-30 10:33:39 -0700

    Create MultiIntegrator which has a transition function; change Livebook ballode.m example to use it (no test yet)

---
+++ lib/integrator/multi_integrator.ex
@@ -0,0 +1,91 @@
+defmodule Integrator.MultiIntegrator do
+  @moduledoc false
+
+  alias Integrator.AdaptiveStepsize
+
+  @type integration_status :: :halt | :continue | :completed
+  @zero_tolerance 1.0e-07
+
+  @type t :: %__MODULE__{
+          event_t: [Nx.t()],
+          event_x: [Nx.t()],
+          transition_x: [Nx.t()],
+          integrations: [AdaptiveStepsize.t()],
+          integration_status: integration_status()
+        }
+
+  defstruct event_t: [],
+            event_x: [],
+            transition_x: [],
+            integrations: [],
+            integration_status: :continue
+
+  @spec integrate(
+          ode_fn :: fun(),
+          event_fn :: fun(),
+          transition_fn :: fun(),
+          t_start :: Nx.t(),
+          t_end :: Nx.t(),
+          x0 :: Nx.t(),
+          opts :: Keyword.t()
+        ) :: t()
+  def integrate(ode_fn, event_fn, transition_fn, t_start, t_end, x0, opts) do
+    multi = %__MODULE__{}
+    opts = opts |> Keyword.merge(event_fn: event_fn)
+    integrate_next_segment(multi, :continue, ode_fn, transition_fn, Nx.to_number(t_start), t_end, x0, opts)
+  end
+
+  @spec integrate_next_segment(
+          multi :: t(),
+          status :: integration_status(),
+          ode_fn :: fun(),
+          transition_fn :: fun(),
+          t_start :: Nx.t(),
+          t_end :: Nx.t(),
+          x0 :: Nx.t(),
+          opts :: Keyword.t()
+        ) :: t()
+  defp integrate_next_segment(multi, _status, _ode_fn, _transition_fn, t_start, t_end, _x0, _opts)
+       when abs(t_start - t_end) < @zero_tolerance or t_start > t_end do
+    %{multi | integration_status: :completed}
+    |> reverse_results()
+  end
+
+  # defp integrate_next_segment(multi, :halt, _ode_fn, _transition_fn, _t_start, _t_end, _x0, _opts) do
+  #   %{multi | integration_status: :halt}
+  #   |> reverse_results()
+  # end
+
+  defp integrate_next_segment(multi, _status, ode_fn, transition_fn, t_start, t_end, x0, opts) do
+    integration = Integrator.integrate(ode_fn, [t_start, t_end], x0, opts)
+    new_t_start = List.last(integration.output_t)
+    last_x = List.last(integration.output_x)
+
+    new_x0 = transition_fn.(new_t_start, last_x)
+
+    multi = multi |> update(new_t_start, last_x, new_x0, integration.terminal_event)
+    integrate_next_segment(multi, :continue, ode_fn, transition_fn, Nx.to_number(new_t_start), t_end, new_x0, opts)
+  end
+
+  @spec reverse_results(t()) :: t()
+  defp reverse_results(multi) do
+    %{
+      multi
+      | event_t: Enum.reverse(multi.event_t),
+        event_x: Enum.reverse(multi.event_x),
+        transition_x: Enum.reverse(multi.transition_x),
+        integrations: Enum.reverse(multi.integrations)
+    }
+  end
+
+  @spec update(t(), Nx.t(), Nx.t(), Nx.t(), integration_status()) :: t()
+  defp update(multi, new_t_start, last_x, new_x0, integration_event) do
+    %{
+      multi
+      | integration_status: integration_event,
+        event_t: [new_t_start | multi.event_t],
+        event_x: [last_x | multi.event_x],
+        transition_x: [new_x0 | multi.transition_x]
+    }
+  end
+end
---
+++ test/integrator/multi_integrator_test.exs
@@ -0,0 +1,9 @@
+defmodule Integrator.MultiIntegratorTest do
+  @moduledoc false
+  use Integrator.TestCase
+
+  describe "ballode.m" do
+    test "works" do
+    end
+  end
+end
