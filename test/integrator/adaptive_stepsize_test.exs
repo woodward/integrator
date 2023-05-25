88a312b3f1b7808f290db8796f23d3cfe4bec2c2
Greg Woodward <woodward@users.noreply.github.com>
2023-05-25 07:38:57 -0700

    Create a test which shows the starting_stepsize goes to zero for Nx.type :f16

--- test/integrator/adaptive_stepsize_test.exs
+++ test/integrator/adaptive_stepsize_test.exs
@@ -944,6 +944,34 @@ defmodule Integrator.AdaptiveStepsizeTest do
       starting_stepsize = AdaptiveStepsize.starting_stepsize(order, ode_fn, t0, x0, abs_tol, rel_tol, opts)
       assert_all_close(starting_stepsize, Nx.tensor(0.001472499532027109, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
     end
+
+    test "does NOT work for precision :f16" do
+      order = 5
+      t0 = ~V[  0.0  ]f16
+      x0 = ~V[  2.0  0.0  ]f16
+      abs_tol = Nx.tensor(1.0e-06, type: :f16)
+      rel_tol = Nx.tensor(1.0e-03, type: :f16)
+      opts = [norm_control: false]
+      ode_fn = &SampleEqns.van_der_pol_fn/2
+
+      starting_stepsize = AdaptiveStepsize.starting_stepsize(order, ode_fn, t0, x0, abs_tol, rel_tol, opts)
+
+      zero_stepsize_which_is_bad = Nx.tensor(0.0, type: :f16)
+
+      assert_all_close(starting_stepsize, zero_stepsize_which_is_bad, atol: 1.0e-14, rtol: 1.0e-14)
+
+      # The starting_stepsize is zero because d2 goes to infinity:
+      # abs_rel_norm = abs_rel_norm(xh_minus_x, xh_minus_x, x_zeros, abs_tol, rel_tol, opts)
+      # Values for abs_rel_norm and h0 captured from Elixir output:
+      abs_rel_norm = Nx.tensor(999.5, type: :f16)
+      h0 = Nx.tensor(0.01000213623046875, type: :f16)
+      one = Nx.tensor(1, type: :f16)
+
+      #  d2 = one / h0 * abs_rel_norm(xh_minus_x, xh_minus_x, x_zeros, abs_tol, rel_tol, opts)
+      d2 = Nx.divide(one, h0) |> Nx.multiply(abs_rel_norm)
+      # d2 being infinity causes the starting_stepsize to be zero:
+      assert_all_close(d2, Nx.Constants.infinity(), atol: 1.0e-14, rtol: 1.0e-14)
+    end
   end
 
   describe "validating all args" do
