8b769c69eb28edcc4b50b57210b3255f0f18a260
Greg Woodward <woodward@users.noreply.github.com>
2024-12-31 13:55:50 -0800

    Switch to using Nx.f64(), Nx.f32(), etc., in NonLinearEqnRootRefactorTest

--- test/integrator/non_linear_eqn_root_refactor_test.exs
+++ test/integrator/non_linear_eqn_root_refactor_test.exs
@@ -65,13 +65,13 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
       # Algorithm converged.
       # x = 3.141592653589795
 
-      x0 = Nx.tensor(3.0, type: :f64)
-      x1 = Nx.tensor(4.0, type: :f64)
+      x0 = Nx.f64(3.0)
+      x1 = Nx.f64(4.0)
 
       result = NonLinearEqnRootRefactor.find_zero(&TestFunctions.sin/2, x0, x1, [])
 
       # Expected value is from Octave:
-      expected_x = Nx.tensor(3.141592653589795, type: :f64)
+      expected_x = Nx.f64(3.141592653589795)
       assert_all_close(result.c, expected_x, atol: 1.0e-14, rtol: 1.0e-14)
       assert_all_close(result.fx, 0.0, atol: 1.0e-14, rtol: 1.0e-14)
 
@@ -81,25 +81,25 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
 
       {x_low, x_high} = NonLinearEqnRootRefactor.bracket_x(result)
       # Expected values are from Octave:
-      assert_all_close(x_low, Nx.tensor(3.141592653589793, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(x_high, Nx.tensor(3.141592653589795, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(x_low, Nx.f64(3.141592653589793), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(x_high, Nx.f64(3.141592653589795), atol: 1.0e-14, rtol: 1.0e-14)
 
       {y1, y2} = NonLinearEqnRootRefactor.bracket_fx(result)
       # Expected values are from Octave:
-      assert_all_close(y1, Nx.tensor(1.224646799147353e-16, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(y2, Nx.tensor(-2.097981369335578e-15, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(y1, Nx.f64(1.224646799147353e-16), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(y2, Nx.f64(-2.097981369335578e-15), atol: 1.0e-14, rtol: 1.0e-14)
     end
 
     test "sine function - works if initial values are swapped" do
-      x0 = Nx.tensor(4.0, type: :f64)
-      x1 = Nx.tensor(3.0, type: :f64)
+      x0 = Nx.f64(4.0)
+      x1 = Nx.f64(3.0)
 
       result = NonLinearEqnRootRefactor.find_zero(&TestFunctions.sin/2, x0, x1, [])
 
       # Expected value is from Octave:
-      expected_x = Nx.tensor(3.141592653589795, type: :f64)
+      expected_x = Nx.f64(3.141592653589795)
       assert_all_close(result.c, expected_x, atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(result.fx, Nx.tensor(0.0, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(result.fx, Nx.f64(0.0), atol: 1.0e-14, rtol: 1.0e-14)
 
       assert Nx.to_number(result.fn_eval_count) == 8
       assert Nx.to_number(result.iteration_count) == 6
@@ -107,13 +107,13 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
 
       {x_low, x_high} = NonLinearEqnRootRefactor.bracket_x(result)
       # Expected values are from Octave:
-      assert_all_close(x_low, Nx.tensor(3.141592653589793, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(x_high, Nx.tensor(3.141592653589795, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(x_low, Nx.f64(3.141592653589793), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(x_high, Nx.f64(3.141592653589795), atol: 1.0e-14, rtol: 1.0e-14)
 
       {y1, y2} = NonLinearEqnRootRefactor.bracket_fx(result)
       # Expected values are from Octave:
-      assert_all_close(y1, Nx.tensor(1.224646799147353e-16, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(y2, Nx.tensor(-2.097981369335578e-15, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(y1, Nx.f64(1.224646799147353e-16), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(y2, Nx.f64(-2.097981369335578e-15), atol: 1.0e-14, rtol: 1.0e-14)
     end
 
     test "sine function - raises an error if invalid initial bracket - positive sine" do
@@ -163,8 +163,8 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
       #   octave> x1 = 4;
       #   octave> x = fzero(fun, [x0, x1])
 
-      x0 = Nx.tensor(3.0, type: :f64)
-      x1 = Nx.tensor(4.0, type: :f64)
+      x0 = Nx.f64(3.0)
+      x1 = Nx.f64(4.0)
 
       {:ok, pid} = DataCollector.start_link()
       output_fn = &DataCollector.add_data(pid, &1)
@@ -172,20 +172,20 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
       opts = [nonlinear_eqn_root_output_fn: output_fn]
 
       result = NonLinearEqnRootRefactor.find_zero(&TestFunctions.sin/2, x0, x1, [], opts)
-      assert_all_close(result.x, Nx.tensor(3.1415926535897936, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(result.fx, Nx.tensor(-3.216245299353273e-16, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(result.x, Nx.f64(3.1415926535897936), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(result.fx, Nx.f64(-3.216245299353273e-16), atol: 1.0e-14, rtol: 1.0e-14)
 
       data = DataCollector.get_data(pid)
       assert length(data) == 6
 
       # From Octave:
       converging_t_data = [
-        Nx.tensor(3.157162792479947, type: :f64),
-        Nx.tensor(3.141281736699444, type: :f64),
-        Nx.tensor(3.141592614571824, type: :f64),
-        Nx.tensor(3.141592692610915, type: :f64),
-        Nx.tensor(3.141592653589793, type: :f64),
-        Nx.tensor(3.141592653589795, type: :f64)
+        Nx.f64(3.157162792479947),
+        Nx.f64(3.141281736699444),
+        Nx.f64(3.141592614571824),
+        Nx.f64(3.141592692610915),
+        Nx.f64(3.141592653589793),
+        Nx.f64(3.141592653589795)
       ]
 
       t_data = data |> Enum.map(& &1.x)
@@ -236,10 +236,10 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
       nx_options = NonLinearEqnRootRefactor.convert_to_nx_options(opts)
       assert %NxOptions{} = nx_options
 
-      assert_all_close(nx_options.machine_eps, Nx.tensor(0.1, type: :f32), atol: 1.0e-16, rtol: 1.0e-16)
+      assert_all_close(nx_options.machine_eps, Nx.f32(0.1), atol: 1.0e-16, rtol: 1.0e-16)
       assert Nx.type(nx_options.machine_eps) == {:f, 32}
 
-      assert_all_close(nx_options.tolerance, Nx.tensor(0.2, type: :f32), atol: 1.0e-16, rtol: 1.0e-16)
+      assert_all_close(nx_options.tolerance, Nx.f32(0.2), atol: 1.0e-16, rtol: 1.0e-16)
       assert Nx.type(nx_options.tolerance) == {:f, 32}
 
       assert nx_options.type == {:f, 32}
@@ -249,14 +249,14 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
     end
 
     test "sine function with single initial value (instead of 2)" do
-      x0 = Nx.tensor(3.0, type: :f64)
+      x0 = Nx.f64(3.0)
 
       result = NonLinearEqnRootRefactor.find_zero_with_single_point(&TestFunctions.sin/2, x0, [])
 
       # Expected value is from Octave:
-      expected_x = Nx.tensor(3.141592653589795, type: :f64)
+      expected_x = Nx.f64(3.141592653589795)
       assert_all_close(result.c, expected_x, atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(result.fx, Nx.tensor(0.0, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(result.fx, Nx.f64(0.0), atol: 1.0e-14, rtol: 1.0e-14)
 
       assert Nx.to_number(result.fn_eval_count) == 11
       assert Nx.to_number(result.iteration_count) == 4
@@ -264,22 +264,22 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
 
       {x_low, x_high} = NonLinearEqnRootRefactor.bracket_x(result)
       # Expected values are from Octave:
-      assert_all_close(x_low, Nx.tensor(3.141592653589793, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(x_high, Nx.tensor(3.141592653589795, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(x_low, Nx.f64(3.141592653589793), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(x_high, Nx.f64(3.141592653589795), atol: 1.0e-14, rtol: 1.0e-14)
 
       {y1, y2} = NonLinearEqnRootRefactor.bracket_fx(result)
       # Expected values are from Octave:
-      assert_all_close(y1, Nx.tensor(1.224646799147353e-16, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(y2, Nx.tensor(-2.097981369335578e-15, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(y1, Nx.f64(1.224646799147353e-16), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(y2, Nx.f64(-2.097981369335578e-15), atol: 1.0e-14, rtol: 1.0e-14)
     end
 
     test "returns pi/2 for cos between 0 & 3 - test from Octave" do
-      x0 = Nx.tensor(0.0, type: :f64)
-      x1 = Nx.tensor(3.0, type: :f64)
+      x0 = Nx.f64(0.0)
+      x1 = Nx.f64(3.0)
 
       result = NonLinearEqnRootRefactor.find_zero(&TestFunctions.cos/2, x0, x1, [])
 
-      expected_x = Nx.divide(Nx.Constants.pi({:f, 64}), Nx.tensor(2.0, type: :f64))
+      expected_x = Nx.divide(Nx.Constants.pi({:f, 64}), Nx.f64(2.0))
       assert_all_close(result.c, expected_x, atol: 1.0e-14, rtol: 1.0e-14)
     end
 
@@ -287,38 +287,38 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
       # Octave (this code is at the bottom of fzero.m):
       #   fun = @(x) x^(1/3) - 1e-8
       #   fzero(fun, [0.0, 1.0])
-      x0 = Nx.tensor(0.0, type: :f64)
-      x1 = Nx.tensor(1.0, type: :f64)
+      x0 = Nx.f64(0.0)
+      x1 = Nx.f64(1.0)
       zero_fn = &TestFunctions.pow_fn/2
 
       result = NonLinearEqnRootRefactor.find_zero(zero_fn, x0, x1, [])
 
       # Expected values are from Octave:
-      assert_all_close(result.x, Nx.tensor(3.108624468950438e-16, type: :f64), atol: 1.0e-24, rtol: 1.0e-24)
-      assert_all_close(result.fx, Nx.tensor(6.764169935169993e-06, type: :f64), atol: 1.0e-22, rtol: 1.0e-22)
+      assert_all_close(result.x, Nx.f64(3.108624468950438e-16), atol: 1.0e-24, rtol: 1.0e-24)
+      assert_all_close(result.fx, Nx.f64(6.764169935169993e-06), atol: 1.0e-22, rtol: 1.0e-22)
     end
 
     test "staight line through zero - test from Octave" do
       # Octave (this code is at the bottom of fzero.m):
       #   fun = @(x) x
       #   fzero(fun, 0)
-      x0 = Nx.tensor(0.0, type: :f64)
+      x0 = Nx.f64(0.0)
       zero_fn = &TestFunctions.straight_line_through_zero/2
 
       result = NonLinearEqnRootRefactor.find_zero_with_single_point(zero_fn, x0, [])
 
-      assert_all_close(result.x, Nx.tensor(0.0, type: :f64), atol: 1.0e-24, rtol: 1.0e-24)
-      assert_all_close(result.fx, Nx.tensor(0.0, type: :f64), atol: 1.0e-24, rtol: 1.0e-24)
+      assert_all_close(result.x, Nx.f64(0.0), atol: 1.0e-24, rtol: 1.0e-24)
+      assert_all_close(result.fx, Nx.f64(0.0), atol: 1.0e-24, rtol: 1.0e-24)
     end
 
     test "staight line through zero offset by one - test from Octave" do
-      x0 = Nx.tensor(0.0, type: :f64)
+      x0 = Nx.f64(0.0)
       zero_fn = &TestFunctions.straight_line_offset_by_one/2
 
       result = NonLinearEqnRootRefactor.find_zero_with_single_point(zero_fn, x0, [])
 
-      assert_all_close(result.x, Nx.tensor(-1.0, type: :f64), atol: 1.0e24, rtol: 1.0e24)
-      assert_all_close(result.fx, Nx.tensor(0.0, type: :f64), atol: 1.0e24, rtol: 1.0e24)
+      assert_all_close(result.x, Nx.f64(-1.0), atol: 1.0e24, rtol: 1.0e24)
+      assert_all_close(result.fx, Nx.f64(0.0), atol: 1.0e24, rtol: 1.0e24)
     end
 
     test "polynomial" do
@@ -329,18 +329,18 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
 
       result = NonLinearEqnRootRefactor.find_zero(zero_fn, 0.5, 1.5, [], type: :f64)
 
-      assert_all_close(result.x, Nx.tensor(1.0, type: :f64), atol: 1.0e24, rtol: 1.0e24)
-      assert_all_close(result.fx, Nx.tensor(0.0, type: :f64), atol: 1.0e24, rtol: 1.0e24)
+      assert_all_close(result.x, Nx.f64(1.0), atol: 1.0e24, rtol: 1.0e24)
+      assert_all_close(result.fx, Nx.f64(0.0), atol: 1.0e24, rtol: 1.0e24)
 
       result = NonLinearEqnRootRefactor.find_zero(zero_fn, 3.5, 1.5, [], type: :f64)
 
-      assert_all_close(result.x, Nx.tensor(3.0, type: :f64), atol: 1.0e24, rtol: 1.0e24)
-      assert_all_close(result.fx, Nx.tensor(0.0, type: :f64), atol: 1.0e24, rtol: 1.0e24)
+      assert_all_close(result.x, Nx.f64(3.0), atol: 1.0e24, rtol: 1.0e24)
+      assert_all_close(result.fx, Nx.f64(0.0), atol: 1.0e24, rtol: 1.0e24)
     end
 
     test "ballode - first bounce" do
-      t0 = Nx.tensor([2.898648469921000], type: :f64)
-      t1 = Nx.tensor([4.294180317944318], type: :f64)
+      t0 = Nx.f64([2.898648469921000])
+      t1 = Nx.f64([4.294180317944318])
 
       x = ~MAT[
            1.676036011799988e+01  -4.564518118928532e+00
@@ -356,16 +356,16 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
       zero_fn_args = [t0, t1, x, k_vals]
 
       # Same values as in ballode definition above:
-      t0 = Nx.tensor(2.898648469921000, type: :f64)
-      t1 = Nx.tensor(4.294180317944318, type: :f64)
+      t0 = Nx.f64(2.898648469921000)
+      t1 = Nx.f64(4.294180317944318)
 
       result = NonLinearEqnRootRefactor.find_zero(zero_fn, t0, t1, zero_fn_args)
 
       # Expected value is from Octave:
-      expected_x = Nx.tensor(4.077471967380223, type: :f64)
+      expected_x = Nx.f64(4.077471967380223)
       assert_all_close(result.c, expected_x, atol: 1.0e-14, rtol: 1.0e-14)
       # This should be close to zero because we found the zero root:
-      assert_all_close(result.fx, Nx.tensor(0.0, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(result.fx, Nx.f64(0.0), atol: 1.0e-14, rtol: 1.0e-14)
 
       assert Nx.to_number(result.fn_eval_count) == 7
       assert Nx.to_number(result.iteration_count) == 5
@@ -373,20 +373,20 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
 
       {x__low, x_high} = NonLinearEqnRootRefactor.bracket_x(result)
       # Expected values are from Octave; note that these are the same except in the last digit:
-      assert_all_close(x__low, Nx.tensor(4.077471967380224, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(x_high, Nx.tensor(4.077471967380227, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(x__low, Nx.f64(4.077471967380224), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(x_high, Nx.f64(4.077471967380227), atol: 1.0e-14, rtol: 1.0e-14)
       # Octave:
       # 4.077471967380223
       # 4.077471967380223
 
       {y_1, y2} = NonLinearEqnRootRefactor.bracket_fx(result)
-      assert_all_close(y_1, Nx.tensor(0.0, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
-      assert_all_close(y2, Nx.tensor(0.0, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(y_1, Nx.f64(0.0), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(y2, Nx.f64(0.0), atol: 1.0e-14, rtol: 1.0e-14)
       # In Octave:
       # [0, 0]
 
       x_out = TestFunctions.ballode(result.c, zero_fn_args)
-      assert_all_close(Nx.to_number(x_out), Nx.tensor(0.0, type: :f64), atol: 1.0e-14, rtol: 1.0e-14)
+      assert_all_close(Nx.to_number(x_out), Nx.f64(0.0), atol: 1.0e-14, rtol: 1.0e-14)
     end
   end
 
@@ -414,19 +414,19 @@ defmodule Integrator.NonLinearEqnRootRefactorTest do
 
   describe "convert_arg_to_nx_type" do
     test "passes through tensors (if they are of the correct type)" do
-      arg = Nx.tensor(1.0, type: :f64)
-      assert NonLinearEqnRootRefactor.convert_arg_to_nx_type(arg, {:f, 64}) == Nx.tensor(1.0, type: :f64)
+      arg = Nx.f64(1.0)
+      assert NonLinearEqnRootRefactor.convert_arg_to_nx_type(arg, {:f, 64}) == Nx.f64(1.0)
     end
 
     test "converts floats to tensors of the appropriate type" do
       arg = 1.0
-      assert NonLinearEqnRootRefactor.convert_arg_to_nx_type(arg, {:f, 32}) == Nx.tensor(1.0, type: :f32)
+      assert NonLinearEqnRootRefactor.convert_arg_to_nx_type(arg, {:f, 32}) == Nx.f32(1.0)
 
-      assert NonLinearEqnRootRefactor.convert_arg_to_nx_type(arg, {:f, 64}) == Nx.tensor(1.0, type: :f64)
+      assert NonLinearEqnRootRefactor.convert_arg_to_nx_type(arg, {:f, 64}) == Nx.f64(1.0)
     end
 
     test "raises an exception if you try to cast a tensor to a different type" do
-      arg = Nx.tensor(1.0, type: :f64)
+      arg = Nx.f64(1.0)
 
       assert_raise TensorTypeError, fn ->
         NonLinearEqnRootRefactor.convert_arg_to_nx_type(arg, {:f, 32})
