86e1a86a02766cc3e82610ea40fb39408d32aba4
Greg Woodward <woodward@users.noreply.github.com>
2025-01-02 06:51:51 -0800

    Delete AdaptiveStepsizeRefactor and associated tests

--- lib/integrator/adaptive_stepsize_refactor.ex
+++
@@ -1,6 +0,0 @@
-defmodule Integrator.AdaptiveStepsizeRefactor do
-  @moduledoc """
-  Integrates a set of ODEs with an adaptive timestep.
-  """
-  # import Nx.Defn
-end
--- test/integrator/adaptive_stepsize_refactor_test.exs
+++
@@ -1,6 +0,0 @@
-defmodule Integrator.AdaptiveStepsizeRefactorTest do
-  @moduledoc false
-  use Integrator.TestCase
-
-  # import Nx, only: :sigils
-end
