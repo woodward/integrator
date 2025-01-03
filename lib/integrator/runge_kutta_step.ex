79daeca141c2fee92a15d83282212bc0f1de352a
Greg Woodward <woodward@users.noreply.github.com>
2025-01-02 20:11:44 -0800

    Create a RungeKuttaStep struct (not in use yet)

---
+++ lib/integrator/runge_kutta_step.ex
@@ -0,0 +1,42 @@
+defmodule Integrator.RungeKuttaStep do
+  @moduledoc """
+  The results of the computation of an individual Runge-Kutta step
+  """
+
+  @derive {Nx.Container,
+   containers: [
+     :t_old,
+     :t_new,
+     #
+     :x_old,
+     :x_new,
+     #
+     :k_vals,
+     :options_comp,
+     :error_estimate
+   ]}
+
+  @type t :: %__MODULE__{
+          t_old: Nx.t(),
+          t_new: Nx.t(),
+          #
+          x_old: Nx.t(),
+          x_new: Nx.t(),
+          #
+          k_vals: Nx.t(),
+          options_comp: Nx.t(),
+          error_estimate: Nx.t()
+        }
+
+  defstruct [
+    :t_old,
+    :t_new,
+    #
+    :x_old,
+    :x_new,
+    #
+    :k_vals,
+    :options_comp,
+    :error_estimate
+  ]
+end
