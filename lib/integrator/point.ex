07bf3833f46104b1781ef9fba717524bff63c729
Greg Woodward <woodward@users.noreply.github.com>
2025-01-03 18:01:11 -0800

    Create Point struct, and a way to create them from t & x.  It's not defn yet, though

---
+++ lib/integrator/point.ex
@@ -0,0 +1,39 @@
+defmodule Integrator.Point do
+  @moduledoc """
+  Strips out the t and x values from a `RungeKutta.Step` into a form that can be used for various
+  output purposes
+  """
+
+  import Nx.Defn
+
+  alias Integrator.Utils
+
+  @derive {Nx.Container,
+           containers: [
+             :t,
+             :x
+           ]}
+
+  @type t :: %__MODULE__{
+          t: Nx.t(),
+          x: Nx.t()
+        }
+
+  defstruct [
+    :t,
+    :x
+  ]
+
+  @spec points_from_t_and_x(Nx.t(), Nx.t()) :: t()
+  deftransform points_from_t_and_x(t, x) do
+    # Change this to a defn somehow and use Nx properly!
+
+    x_cols = Utils.columns_as_list(x, 0)
+    t_list = Nx.to_list(t)
+
+    Enum.zip(t_list, x_cols)
+    |> Enum.map(fn {t, x} ->
+      %__MODULE__{t: t, x: Nx.to_list(x)}
+    end)
+  end
+end
---
+++ test/integrator/point_test.exs
@@ -0,0 +1,25 @@
+defmodule Integrator.PointTest do
+  @moduledoc false
+  use Integrator.TestCase, async: true
+
+  import Nx, only: :sigils
+
+  alias Integrator.Point
+
+  describe "new/2" do
+    test "creates a list of Points from t and x values" do
+      t = ~VEC[0.1 0.2]f64
+      x = ~MAT[
+           1.0   2.0
+           3.0   4.0
+      ]f64
+
+      points = Point.points_from_t_and_x(t, x)
+
+      [point1, point2] = points
+
+      assert point1 == %Point{t: 0.1, x: [1.0, 3.0]}
+      assert point2 == %Point{t: 0.2, x: [2.0, 4.0]}
+    end
+  end
+end
