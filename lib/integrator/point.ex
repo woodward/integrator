c68d6d69e19becf84332dfa11c1e7db69256531e
Greg Woodward <woodward@users.noreply.github.com>
2025-01-04 07:57:45 -0800

    Switch Point.convert_to_points/2 to use a case statement rather than a nested if statement

--- lib/integrator/point.ex
+++ lib/integrator/point.ex
@@ -24,42 +24,38 @@ defmodule Integrator.Point do
 
   @spec convert_to_points(Nx.t(), Nx.t()) :: t()
   defn convert_to_points(t, x) do
-    size = Nx.size(t)
     x_t = Nx.transpose(x)
 
     # Make this more Nx-ey somehow, and less brute-force!  There should be a general way to do this
-    # that's not hard-wired to sizes 1-4. And at the very least, get a case statement to work, rather
-    # than these nasty nested if statements!
+    # that's not hard-wired to sizes 1-4. But how???
+    case Nx.size(t) do
+      1 ->
+        {%__MODULE__{t: t[0], x: x_t[0]}}
 
-    if size == 1 do
-      {%__MODULE__{t: t[0], x: x_t[0]}}
-    else
-      if size == 2 do
+      2 ->
         {
           %__MODULE__{t: t[0], x: x_t[0]},
           %__MODULE__{t: t[1], x: x_t[1]}
         }
-      else
-        if size == 3 do
-          {
-            %__MODULE__{t: t[0], x: x_t[0]},
-            %__MODULE__{t: t[1], x: x_t[1]},
-            %__MODULE__{t: t[2], x: x_t[2]}
-          }
-        else
-          if size == 4 do
-            {
-              %__MODULE__{t: t[0], x: x_t[0]},
-              %__MODULE__{t: t[1], x: x_t[1]},
-              %__MODULE__{t: t[2], x: x_t[2]},
-              %__MODULE__{t: t[3], x: x_t[3]}
-            }
-          else
-            # Currently this is hard-wired to only go up to 4:
-            raise_size_exception(size)
-          end
-        end
-      end
+
+      3 ->
+        {
+          %__MODULE__{t: t[0], x: x_t[0]},
+          %__MODULE__{t: t[1], x: x_t[1]},
+          %__MODULE__{t: t[2], x: x_t[2]}
+        }
+
+      4 ->
+        {
+          %__MODULE__{t: t[0], x: x_t[0]},
+          %__MODULE__{t: t[1], x: x_t[1]},
+          %__MODULE__{t: t[2], x: x_t[2]},
+          %__MODULE__{t: t[3], x: x_t[3]}
+        }
+
+      size ->
+        # Currently this function is hard-wired to only allow up to size 4:
+        raise_size_exception(size)
     end
   end
 
