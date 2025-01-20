0476047200d3ed62ee9c6168b45613a39cdcef26
Greg Woodward <woodward@users.noreply.github.com>
2025-01-20 12:25:37 -0800

    Get rid of AdaptiveStepsize (in preparation for renaming AdaptiveStepsizeRefactor to AdaptiveStepsize)

--- lib/integrator/adaptive_stepsize/arg_precision_error.ex
+++
@@ -1,9 +0,0 @@
-defmodule Integrator.AdaptiveStepsize.ArgPrecisionError do
-  @moduledoc false
-
-  defexception message: "argument precision error",
-               invalid_argument: nil,
-               argument_name: nil,
-               expected_precision: nil,
-               actual_precision: nil
-end
--- lib/integrator/adaptive_stepsize/max_errors_exceeded_error.ex
+++
@@ -1,8 +0,0 @@
-defmodule Integrator.AdaptiveStepsize.MaxErrorsExceededError do
-  @moduledoc false
-
-  defexception message: "Too many errors",
-               error_count: 0,
-               max_number_of_errors: 0,
-               step: nil
-end
